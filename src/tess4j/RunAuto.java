1a118983cf146b433edc6fa14d1a44f4bfc5713d
ethanrong <ethanrong@10.248.45.191>
2020-09-16 14:24:24 +1000

    Added class for running autobattle on auto

---
+++ src/tess4j/RunAuto.java
@@ -0,0 +1,26 @@
+package tess4j;
+
+public class RunAuto {
+    public void StartRun() {
+        // user to press "repeat battle" run, assume they have enough energy //
+        
+        /*
+        while (run == true) {
+            if (image is "10/10") {
+                commenceSellingRunes();
+                click repeat battle;
+                run = false;
+            }
+            else if (image is out of energy) {
+                click energy button;
+                refresh energy x120;
+                click repeat battle;
+                run = false;
+            }
+            else if (failed run) {
+                repeat battle;
+            }
+        }
+        */
+    }
+}
